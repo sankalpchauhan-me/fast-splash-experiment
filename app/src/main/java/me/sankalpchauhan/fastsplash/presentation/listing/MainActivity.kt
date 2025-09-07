1ed28e31ae4f551e49943af886d2ba48aa6b376f
Sankalp <sankalpchauhan.me@gmail.com>
2025-09-07 22:35:24 +0530

    feat: added perf tracker lib

--- app/src/main/java/me/sankalpchauhan/fastsplash/presentation/listing/MainActivity.kt
+++ app/src/main/java/me/sankalpchauhan/fastsplash/presentation/listing/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.sankalpchauhan.fastsplash.presentation.listing
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import me.sankalpchauhan.fastsplash.FastSplashApplication
 import me.sankalpchauhan.fastsplash.data.model.Movie
 import me.sankalpchauhan.fastsplash.presentation.base.ui.theme.FastSplashTheme
 import me.sankalpchauhan.fastsplash.presentation.listing.widgets.MovieCard
@@ -29,11 +31,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fcpTrace = (applicationContext as FastSplashApplication).fcp
         enableEdgeToEdge()
         setContent {
             val uiState by mainViewModel.uiState.collectAsState()
             var userQuery by remember { mutableStateOf("") }
-            
+            fcpTrace.stopTrace()
+            Log.d("PERF", "Total First Content Paint Time = ${fcpTrace.getDuration()}")
             FastSplashTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
